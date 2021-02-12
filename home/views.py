b6bb94118efbc006dbb61f8ec754b8f0b8541654
DanielJMaia <djohnsonmaia@gmail.com>
2021-02-12 15:21:07 +0000

    modified website layout, created landing page animation

--- home/views.py
+++ home/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    template = 'home/index.html'
+    
+    return render(request, template)
--- my_site/settings.py
+++ my_site/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['192.168.50.147']
+ALLOWED_HOSTS = ['192.168.50.147', 'localhost']
 
 
 # Application definition
