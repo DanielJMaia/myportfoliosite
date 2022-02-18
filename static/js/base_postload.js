80128c1ed8558a8917a8e44537fc7da3a5f4304c
DanielJMaia <djohnsonmaia@gmail.com>
2022-02-18 16:37:38 +0000

    added 3s timeout on toasts

--- static/js/base_postload.js
+++ static/js/base_postload.js
@@ -4,3 +4,11 @@ contact.addEventListener("click", scrollToBottom);
 function scrollToBottom() {
     window.scrollTo(0,document.body.scrollHeight);
 }
+
+// Django Messages Code
+$('.toast').toast('show'); //Required to change opacity CSS of message container
+//This timeout function hides the message container after 3 seconds
+var message_ele = document.getElementById("message-container-id");
+setTimeout(function(){ 
+    $('.toast').toast('hide');
+}, 3000);
